941d77d2a59b00de2a8940bdc7b7800d050310b5
Natlinux81 <nathaliewenske790@hotmail.com>
2024-10-03 19:02:10 +0200

    try first unit test

--- tests/Application.UnitTest/UnitTest1.cs
+++ tests/Application.UnitTest/UnitTest1.cs
@@ -1,10 +1,46 @@
+using Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace Application.UnitTest;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void AddRssFeed_ShouldAddFeedAndItems()
     {
+        // Arrange
+        var options = new DbContextOptionsBuilder<RssReaderDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase") // Benenne die In-Memory-Datenbank
+            .Options;
 
+        using (var context = new RssReaderDbContext(options))
+        {
+            var rssFeed = new RssFeed
+            {
+                Id = 1,
+                ChannelTitle = "Test Feed",
+                FeedItems = new List<RssFeedItem>
+                {
+                    new RssFeedItem { Id = 1, Title = "First Item", RssFeedId = 1 },
+                    new RssFeedItem { Id = 2, Title = "Second Item", RssFeedId = 1 }
+                }
+            };
+
+            // Act
+            context.RssFeeds.Add(rssFeed);
+            context.SaveChanges();
+        }
+
+        // Assert
+        using (var context = new RssReaderDbContext(options))
+        {
+            var feed = context.RssFeeds.Include(f => f.FeedItems).FirstOrDefault(f => f.Id == 1);
+            Assert.NotNull(feed);
+            Assert.Equal("Test Feed", feed.ChannelTitle);
+            Assert.Equal(2, feed.FeedItems.Count);
+            Assert.Equal("First Item", feed.FeedItems.First().Title);
+            Assert.Equal("Second Item", feed.FeedItems.Last().Title);
+        }
     }
 }
